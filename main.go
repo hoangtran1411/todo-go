0236688dba8f85a7cf771a31391b3114dfc05405
hoangtran1411 <chi3xitin2010@gmail.com>
2025-10-08 21:27:19 +0700

    add new storage

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 func main() {
 	todos := Todos{}
+	storage := NewStogare[Todos]("todos.json")
+	storage.Load(&todos)
+
 	todos.Add("Buy milk")
 	todos.Add("Buy eggs")
 	todos.Add("Buy bread")
@@ -9,4 +12,5 @@ func main() {
 	todos.Toggle(0)
 
 	todos.Print()
+	storage.Save(todos)
 }
--- storage.go
+++ storage.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type Stogare[T any] struct {
 	FileName string
 }
@@ -8,4 +13,33 @@ func NewStogare[T any](fileName string) *Stogare[T] {
 	return &Stogare[T]{
 		FileName: fileName,
 	}
+}
+
+func (s *Stogare[T]) Save(data T) error {
+	fileData, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(s.FileName, fileData, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Stogare[T]) Load(data *T) error {
+
+	fileData, err := os.ReadFile(s.FileName)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(fileData, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
