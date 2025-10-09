cfb25510f400aa523c75f166054213013dbb3556
hoangtran1411 <chi3xitin2010@gmail.com>
2025-10-09 19:51:58 +0700

    update

--- command.go
+++ command.go
@@ -6,7 +6,7 @@ type CmdFlags struct {
 	Add    string `short:"a" long:"add" description:"Add a new todo"`
 	List   bool   `short:"l" long:"list" description:"List all todos"`
 	Toggle int    `short:"t" long:"toggle" description:"Toggle todo status"`
-	Delete int    `short:"d" long:"delete" description:"Delete a todo"`
+	Del    int    `short:"d" long:"delete" description:"Delete a todo"`
 	Edit   string `short:"e" long:"edit" description:"Edit a todo"`
 }
 
@@ -15,7 +15,7 @@ func NewCmdFlags() *CmdFlags {
 	flag.StringVar(&cf.Add, "add", "", "Add a new todo")
 	flag.BoolVar(&cf.List, "list", false, "List all todos")
 	flag.IntVar(&cf.Toggle, "toggle", -1, "Toggle todo status")
-	flag.IntVar(&cf.Delete, "delete", -1, "Delete a todo")
+	flag.IntVar(&cf.Del, "del", -1, "Delete a todo")
 	flag.StringVar(&cf.Edit, "edit", "", "Edit a todo")
 	flag.Parse()
 	return &cf
