401611d69389a0d4a572873f9294a1d158c057a3
hoangtran1411 <chi3xitin2010@gmail.com>
2025-10-09 20:02:22 +0700

    add flags

--- main.go
+++ main.go
@@ -4,14 +4,7 @@ func main() {
 	todos := Todos{}
 	storage := NewStogare[Todos]("todos.json")
 	storage.Load(&todos)
-
-	todos.Add("Buy milk")
-	todos.Add("Buy eggs")
-	todos.Add("Buy bread")
-
-	todos.Toggle(0)
-
-	todos.Print()
+	cmdFlags := NewCmdFlags()
+	cmdFlags.Execute(&todos)
 	storage.Save(todos)
-	//
 }
